836e44b7a72251cc927015e190013b3ddbf9f253
André Schuhmacher <70041575+SchuhmacherAndre@users.noreply.github.com>
2024-10-12 02:43:20 +0200

    Update src.cpp

--- testvar/src/src.cpp
+++ testvar/src/src.cpp
@@ -1,14 +1,39 @@
 #include <iostream>
+#include <thread>
+#include <atomic>
+#include <chrono>
 
 int main()
 {
     // gen random number 0-255
     std::srand(std::time(0));
-    int randomNum = std::rand() % 256;
+    std::atomic<int> randomNum(std::rand() % 256);
 
-    // cast to ptr to truncate prefix 0s
     std::cout << "addr_: 0x" << std::hex << reinterpret_cast<uintptr_t>(&randomNum) << std::endl;
-    std::cout << "value: 0x" << std::hex << randomNum << std::endl;
+    std::cout << "value: 0x" << std::hex << randomNum.load() << std::endl;
 
-    return 1;
+    int oldValue = randomNum.load();
+
+    std::thread monitorThread([&]() {
+        while (true)
+        {
+            int newValue = randomNum.load();
+            if (newValue != oldValue)
+            {
+                std::cout << "value: 0x" << std::hex << newValue << std::endl;
+                oldValue = newValue;
+            }
+           
+            std::this_thread::sleep_for(std::chrono::milliseconds(100));
+        }
+        });
+
+   
+    for (;;)
+    {
+        
+    }
+
+    monitorThread.join();
+    return 0;
 }
